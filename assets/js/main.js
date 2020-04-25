4ce91548307bc85d3ef93c0cda158a6983522e83
sbirhan <sbirhan@gmail.com>
2020-04-25 09:40:27 +0200

    second  commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
 var i=0;
-var j=0;
 var firstClass="";
 var secondClass="";
 
 function styling(className){
     i++;
-    j = i % 2;
     if (i == 1) {
         document.getElementById("greetingBox").classList.add(className);
         firstClass = className;
@@ -17,4 +15,3 @@ function styling(className){
         firstClass = secondClass;
     }
 }
-
