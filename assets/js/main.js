f9288a22e18b16c31204048fbd72219d880e4b3d
sbirhan <sbirhan@gmail.com>
2020-04-25 04:43:38 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+var i=0;
+var j=0;
+var firstClass="";
+var secondClass="";
+
+function styling(className){
+    i++;
+    j = i % 2;
+    if (i == 1) {
+        document.getElementById("greetingBox").classList.add(className);
+        firstClass = className;
+    }
+    else {
+        secondClass = className;
+        document.getElementById("greetingBox").classList.remove(firstClass);
+        document.getElementById("greetingBox").classList.add(secondClass);
+        firstClass = secondClass;
+    }
+
+    console.log(i);
+    console.log(j);
+    console.log(firstClass);
+    console.log(secondClass);
+
+}
+
